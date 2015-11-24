c408b207a50f3374ba2b70fd856a0ed423ec4e10
Shujin <cedric.pignard-fernandez@ensiie.fr>
2015-11-24 10:52:57 +0100

    Add InputMannager.cs + Jump.cs Modify InputMananger.asset: up key as secondary jump key Remove TestJump.cs

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// JumpEnum
+public enum JumpEnum {
+	Pressed,
+	StillPressed,
+	Stop
+}
+
+// Receive the events from the user
+public class InputManager : MonoBehaviour {
+
+	// Singleton
+	public static InputManager Instance
+	{
+		get;
+		private set;
+	}
+	private void Awake()
+	{
+		if (Instance != null)
+			return;
+		Instance = this;
+	}
+
+	// Jump Event
+	public delegate void JumpInput (JumpEnum e);
+	public event JumpInput OnJumpInput;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		IsJumpInput();
+	}
+
+	void IsJumpInput () {
+		if (OnJumpInput != null) {
+			if (Input.GetButtonDown("Jump")) {
+				OnJumpInput(JumpEnum.Pressed);
+			}
+			else if (Input.GetButton("Jump")) {
+				OnJumpInput(JumpEnum.StillPressed);
+			}
+			else if (Input.GetButtonUp("Jump")) {
+				OnJumpInput(JumpEnum.Stop);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerJump : MonoBehaviour {
+
+	// Jump properties
+	[SerializeField]
+	private float MaxJumpValue;
+	private float JumpMultiplier;
+
+	[SerializeField]
+	private float MaxJumpChargeTime;
+	private float JumpChargeTime;
+	
+	private Rigidbody2D PlayerRigidBody;
+	
+	private bool IsGrounded;
+	private bool HasAlreadyJumped;
+
+	private InputManager InputManagerComponent;
+	
+	// Use this for initialization
+	void Start () {
+		InputManagerComponent = InputManager.Instance;
+
+		if (InputManagerComponent == null) {
+			Debug.LogWarning("PlayerMove.Start(): InputManagerComponent == null");
+		}
+
+		InputManagerComponent.OnJumpInput += JumpEventHandler;
+
+		InitJumpState();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Initialize the jump properties
+	void InitJumpState () {
+		PlayerRigidBody = GetComponent<Rigidbody2D>();
+		
+		if (PlayerRigidBody == null) {
+			Debug.LogWarning("PlayerMove.InitJumpStata(): rigidbody2D == null");
+		}
+		
+		JumpChargeTime = 0;
+		
+		// Multiplier to give a max value for the jump (100% time <=> 100% jump, linear)
+		JumpMultiplier = MaxJumpValue / MaxJumpChargeTime;
+		
+		IsGrounded = true; // Not clean but if the player begins on a platform (colliding) this allows him to jump
+		HasAlreadyJumped = false;
+	}
+
+	void JumpEventHandler (JumpEnum e) {
+		if (e == JumpEnum.Pressed && IsGrounded) {
+			Jump();
+			HasAlreadyJumped = false;
+		}
+		else if (e == JumpEnum.StillPressed && JumpChargeTime < MaxJumpChargeTime && !HasAlreadyJumped) {
+			Jump();
+		}
+		else if (e == JumpEnum.Stop) {
+			JumpChargeTime = 0f;
+			HasAlreadyJumped = true;
+		}
+	}
+
+	// Add a bit of velocity to the jumper each time this method is called
+	void Jump () {
+		JumpChargeTime += Time.deltaTime;
+		
+		JumpChargeTime = Mathf.Min(JumpChargeTime, MaxJumpChargeTime);
+		
+		// Rule of three to have a linear jump
+		PlayerRigidBody.velocity += new Vector2(0f, JumpChargeTime * JumpMultiplier / MaxJumpChargeTime);
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.collider.tag == "Ground") {
+			IsGrounded = true;
+		}
+	}
+	
+	void OnCollisionExit2D (Collision2D other) {
+		if (other.collider.tag == "Ground") {
+			IsGrounded = false;
+		}
+	}
+}
